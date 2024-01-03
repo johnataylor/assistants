ac6d5dbb4a72ae13917a1fd188eec4dbb83bf858
John Taylor <johtaylo@microsoft.com>
2024-01-02 22:03:47 -0800

    list steps

---
+++ demo/listSteps.js
@@ -0,0 +1,15 @@
+
+const threadId = process.argv[2];
+const runId = process.argv[3];
+
+import { OpenAI } from "openai";
+
+var baseURL = 'https://api.openai.com/v1';
+//var baseURL = 'http://localhost:3000/v1';
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, baseURL: baseURL });
+
+const steps = await openai.beta.threads.runs.steps.list(threadId, runId);
+
+console.log(JSON.stringify(steps, null, 2));
+
