6963830d69298a7699bfce530299a229928ca942
John Taylor <johtaylo@microsoft.com>
2024-01-04 16:59:20 -0800

    functions test

--- functions/main.js
+++ functions/main.js
@@ -9,8 +9,8 @@ const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
 
 import { OpenAI } from "openai";
 
-var baseURL = 'https://api.openai.com/v1'
-//var baseURL = 'http://localhost:3000/v1'
+//var baseURL = 'https://api.openai.com/v1'
+var baseURL = 'http://localhost:3000/v1'
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, baseURL: baseURL });
 
