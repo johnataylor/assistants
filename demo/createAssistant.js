6f00487eee4615ffe455ced2415ee7d02bd11d69
John Taylor <johtaylo@microsoft.com>
2024-01-16 18:10:37 -0800

    testing

--- demo/createAssistant.js
+++ demo/createAssistant.js
@@ -1,8 +1,8 @@
 
 import { OpenAI } from "openai";
 
-var baseURL = 'https://api.openai.com/v1'
-//var baseURL = 'http://localhost:3000/v1'
+//var baseURL = 'https://api.openai.com/v1'
+var baseURL = 'http://localhost:3000/v1'
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, baseURL: baseURL });
 
@@ -10,7 +10,7 @@ const assistant = await openai.beta.assistants.create({
   name: "Math Tutor",
   instructions: "You are a personal math tutor. Write and run code to answer math questions.",
   tools: [{ type: "code_interpreter" }],
-  model: "gpt-4-1106-preview"
+  //model: "gpt-4-1106-preview"
 });
 
 console.log(assistant.id);
